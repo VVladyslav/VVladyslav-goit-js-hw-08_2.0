4e878448ef2555f46329b93f0265ba63f8f0c756
VVladyslav <VVVasylyshyn@gmail.com>
2023-01-19 16:26:05 +0200

    hw_08 1st task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import { galleryItems } from "./gallery-items.js";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Change code below this line
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems.reduce(
+  (acc, { original, preview, description }) =>
+    acc +
+    `<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}"/></a>`,
+  ""
+);
+gallery.insertAdjacentHTML("afterbegin", markup);
 
-console.log(galleryItems);
+let instance = new SimpleLightbox(".gallery.gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
+instance.on(`show.instance`);
